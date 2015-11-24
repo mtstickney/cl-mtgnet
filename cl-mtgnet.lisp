1b01fbac2ea07bb566e30dbb0e084b5cf23a26ac
Matthew Stickney <fiendishlinux@gmail.com>
2015-11-24 14:14:05 -0500

    Chain send errors to corresponding reads.
    A read depends on the send happening successfully, so the read op needs
    to depend on the success of the send, which means chaining the promise
    together. This is likely the cause of previous issues in reconnect
    handlers with phantom reads happening after a connection shutdown.

--- cl-mtgnet.lisp
+++ cl-mtgnet.lisp
@@ -199,8 +199,8 @@ before returning."
        ;; let WITH-BATCH-CALLS trigger the read and complete it.
        (blackbird:with-promise (resolve reject :resolve-fn resolve-it)
          (push (cons call resolve-it) *rpc-batch*)))
-      (t (send-request con (list call))
-         (rpc-call-promise con call)))))
+      (t (blackbird:wait (send-request con (list call))
+           (rpc-call-promise con call))))))
 
 (defmacro with-batch-calls ((con &optional (batch-req nil batch-supplied-p)) &body body)
   "Arrange for RPC calls in this block to collect their calls into one
@@ -209,13 +209,13 @@ request, which will be sent at the end of the block."
      (declare (special *rpc-batch*))
      ,@body
      (let ((batch (reverse *rpc-batch*)))
-       (send-request ,con (mapcar #'first batch))
-       (blackbird:all (mapcar (lambda (cell)
-                                (let ((resolver (cdr cell))
-                                      (promise (rpc-call-promise ,con (car cell))))
-                                  (funcall resolver promise)
-                                  promise))
-                              batch)))))
+       (blackbird:wait (send-request ,con (mapcar #'first batch))
+         (blackbird:all (mapcar (lambda (cell)
+                                  (let ((resolver (cdr cell))
+                                        (promise (rpc-call-promise ,con (car cell))))
+                                    (funcall resolver promise)
+                                    promise))
+                                batch))))))
 
 (defmacro bind-args ((arg-var encoder-var &optional typespec-var) arg-obj &body body)
   (let ((typespec-var (if typespec-var typespec-var (gensym "TYPESPEC"))))
